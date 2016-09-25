def3ea0973e7dfde6e3ddb210aae6b1c98f6461e
Artyom Likhachev <alikhach@itcollege.ee>
2016-09-25 11:54:29 +0300

    praktikum1 esimene osa

---
+++ Praktikum01Solution/Praktikum01/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Praktikum01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Luua meetod, mis küsib kasutaja käest arvu ning teeb seda seni kaua, kuni kasutaja on andnud korrektse sisendi
+            // KysiKasutajaltArv("Palun sisesta number:");
+
+            //Luua meetod, mis küsib kasutajalt vanust ning tagastab, kas tegemist on täisealise kasutajaga
+            bool vanusekontroll = KasOnaisealine(KysiKasutajaltArv("Palun sisestage oma vanus:"));
+
+
+        }
+        //Luua meetod, mis küsib kasutaja käest arvu ning teeb seda seni kaua, kuni kasutaja on andnud korrektse sisendi
+        ////1. Mida peaks meetid tagastama?
+        //Taisarv(int)
+        //2.Mis on meetodi nimi?
+        //KysiKasutajaltArv
+        //3.Kas meetod vajab sisendparametrid?
+        //jah
+        static int KysiKasutajaltArv(string tekst)
+        {
+            Console.WriteLine(tekst);
+            string vastus = Console.ReadLine();
+            int a;
+            if (int.TryParse(vastus, out a) == true)
+            {
+                return a;
+            }
+            else
+            {
+                return KysiKasutajaltArv(tekst);
+            }
+        }
+
+        //Luua meetod, mis küsib kasutajalt vanust ning tagastab, kas tegemist on täisealise kasutajaga
+        //1.Mida peaks meetod tagastama?
+        //bool- true/false
+        //2.Mis on meetodi nimi?
+        //3.Kas meetod vajab sisendparametrid?
+        //jah, kasutaja vanus
+
+        static bool KasOnaisealine(int vanus)
+        {
+            return vanus > 17;
+        }
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+https://wiki.itcollege.ee/index.php/Category:Programmeerimine_CSharp_keeles
+järgmiseks pühapäevaks 3 praktikumid tehtud ja saadetud +1p
